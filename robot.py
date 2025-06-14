7818d3e62bc0877c805e74c20283d7da3192b948
Mike Silva <mike.a.silva@gmail.com>
2025-06-14 11:31:34 -0400

    Added solutions to challenge 1 and 2

--- robot.py
+++ robot.py
@@ -13,6 +13,25 @@ Create the following methods:
 - `go`: sets the `power` attribute to a given value
 """
 
+
+class Motor:
+    def __init__(self, name: str, power: float = 0.0) -> None:
+        self.name = name
+        self.power = power
+
+    def get_power(self) -> float:
+        return self.power
+
+    def set_power(self, power: float) -> None:
+        self.power = power
+
+    def stop(self) -> None:
+        self.power = 0.0
+
+    def go(self, power: float) -> None:
+        self.set_power(power)
+
+
 """ 
 CHALLENGE 2
 
@@ -24,3 +43,24 @@ Create the following methods:
 - `go`: sets the power of all motors to a given value
 - `stop`: sets the power of all motors to 0.0
 """
+
+
+class Robot:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.left_front = Motor("left_front")
+        self.left_back = Motor("left_back")
+        self.right_front = Motor("right_front")
+        self.right_back = Motor("right_back")
+
+    def stop(self) -> None:
+        self.left_back.stop()
+        self.left_front.stop()
+        self.right_front.stop()
+        self.right_back.stop()
+
+    def go(self, power: float) -> None:
+        self.left_back.go(power)
+        self.left_front.go(power)
+        self.right_back.go(power)
+        self.right_front.go(power)
